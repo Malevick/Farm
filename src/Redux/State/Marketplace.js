f329e5af7340fe0ab597f7337b3c29d72016c908
Joris Langlois <joris.langlois@knplabs.com>
2020-02-19 15:41:40 +0100

    Fixed marketplace initial state

--- src/Redux/State/Marketplace.js
+++ src/Redux/State/Marketplace.js
@@ -1,30 +1,28 @@
-export const SEED_TYPES = [
-    {
-        name : "wheat",
-        cropTime : 1,
-        sellPrice : 3,
-        buyPrice : 1,
-        waterRequirements : 3
-    },
-    {
-        name : "corn",
-        cropTime : 2,
-        sellPrice : 11,
-        buyPrice : 3,
-        waterRequirements : 9
-    },
-    {
-        name : "barley",
-        cropTime : 3,
-        sellPrice : 20,
-        buyPrice : 5,
-        waterRequirements : 15
-    },
-]
-
 // initial state
 export const INITIAL_STATE = {
-    seedTypes : SEED_TYPES
+    seedTypes : [
+        {
+            name : "wheat",
+            cropTime : 1,
+            sellPrice : 3,
+            buyPrice : 1,
+            waterRequirements : 3
+        },
+        {
+            name : "corn",
+            cropTime : 2,
+            sellPrice : 11,
+            buyPrice : 3,
+            waterRequirements : 9
+        },
+        {
+            name : "barley",
+            cropTime : 3,
+            sellPrice : 20,
+            buyPrice : 5,
+            waterRequirements : 15
+        },
+    ]
 }
 
 export default (state = INITIAL_STATE, action = {}) => {
